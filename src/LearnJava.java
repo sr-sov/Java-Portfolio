2751908a10aa3bb1067043ea814dff8d118cc6b8
joshua-lista <joshua@lista.com.ph>
2023-02-20 15:34:52 +0800

    first commit

---
+++ src/LearnJava.java
@@ -0,0 +1,15 @@
+public class LearnJava {
+    public static void main(String[] args) throws Exception {
+        //storing data in primitive types
+        int a = 1;
+        char b = 'i';
+        long c = 400;
+        double d = 3.2;
+
+        //storing data in non-primitive
+        String name = "Jhin";
+        System.out.println(name.charAt(a));
+        System.out.println(name.toUpperCase());
+
+    }
+}
